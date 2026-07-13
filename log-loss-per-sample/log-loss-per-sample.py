f18ec61096db9c8d3625793061e4168c599bb142
0xcf2f <fawomath@gmail.com>
2026-07-13 20:30:08 +0200

    Solve log-loss-per-sample

--- log-loss-per-sample/log-loss-per-sample.py
+++ log-loss-per-sample/log-loss-per-sample.py
@@ -5,6 +5,6 @@ def log_loss(y_true, y_pred, eps=1e-15):
     Compute per-sample log loss.
     """
     # Write code here
-    yc = np.clip(y_pred, eps, 1 - eps)
+    yc = np.clip(y_pred, eps, 1 - eps) # clip the prediction values
     ll = -1 * (y_true * np.log(yc) + np.subtract(1, y_true) * np.log(np.subtract(1, yc)))
     return list(ll)
